21c507cf0ad0049c258dafc55643e2fadd223f45
Tahmid Islam <tahmidislam1998@gmail.com>
2019-11-17 05:45:47 +0600

    quiz announcment is done,yaaaaaaaaaaay

---
+++ app/src/main/java/com/example/iutassistant/QuizInfo.java
@@ -0,0 +1,10 @@
+package com.example.iutassistant;
+
+public class QuizInfo {
+    String t_id,syllabus,quizDate;
+    QuizInfo(String t_id,String syllabus,String quizDate){
+        this.t_id=t_id;
+        this.quizDate=quizDate;
+        this.syllabus=syllabus;
+    }
+}
