8ccec9150bb09e21abcc9c2cd56b0a279b02fc99
Tahmid Islam <tahmidislam1998@gmail.com>
2019-11-17 13:11:27 +0600

    attendance and class update class is updated

---
+++ app/src/main/java/com/example/iutassistant/ResheduledInfo.java
@@ -0,0 +1,12 @@
+package com.example.iutassistant;
+
+public class ResheduledInfo {
+    String t_id,type,explanation,date;
+    ResheduledInfo(String t_id,String type,String explanation,String date){
+        this.t_id=t_id;
+        this.type=type;
+        this.explanation=explanation;
+        this.date=date;
+
+    }
+}
